0b0339f0521327a76f0975688ec65d967408e2e0
Sid <saytleader@outlook.com>
2023-08-08 22:12:02 +0530

    Update Receiver.hx

--- src/prism/chat/Receiver.hx
+++ src/prism/chat/Receiver.hx
@@ -7,6 +7,12 @@ import haxe.Http;
 import prism.util.structs.Message;
 
 class Receiver {
+	/**
+	 * Gets all messages from the remote server
+	 * @param url URL of the remote server
+	 * @return Array of all the messages
+	 * @since 0.1
+	 */
 	public static function getAll(url:String):Array<Message> {
 		var req = new Http(url);
 		var r:Request = {
@@ -22,17 +28,30 @@ class Receiver {
 		req.onData = function(raw:String) {
 			var data:Request = Json.parse(raw);
 			if (data.status == SUCCESS) {
-				re = data.actions[0].params.data;
+				re = data.response[0];
 			} else if (data.status == FAILURE || data.status == ERROR || data.status == NOT_FOUND || data.status == INVALID) {}
 		}
 		return re;
 	}
 
+	/**
+	 * Gets the latest message from the remote server
+	 * @param url URL of the remote server
+	 * @return The latest message
+	 * @since 0.1
+	 */
 	public static function getLatest(url:String):Message {
 		var g = getAll(url);
 		return g[g.length - 1];
 	}
 
+	/**
+	 * Gets the latest messages from the remote server
+	 * @param url URL of the remote server
+	 * @param count Number of messages to return
+	 * @return Array of messages
+	 * @since 0.1
+	 */
 	public static function getMultiple(url:String, count:Int):Array<Message> {
 		var x = [];
 		var g = getAll(url);
