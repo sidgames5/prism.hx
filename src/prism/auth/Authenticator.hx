8e571970a3d89f7ed699c69eae0b9962d9ab896b
Sid <saytleader@outlook.com>
2023-08-08 18:56:37 +0530

    Update Authenticator.hx

--- src/prism/auth/Authenticator.hx
+++ src/prism/auth/Authenticator.hx
@@ -1,5 +1,9 @@
 package prism.auth;
 
+import haxe.Json;
+import prism.crypto.Encryption;
+import prism.util.structs.Request;
+import sys.Http;
 import haxe.io.Bytes;
 import prism.util.Instructions.*;
 
@@ -11,8 +15,27 @@ class Authenticator {
 	 * @return Key, returns null if the user cannot be validated
 	 * @since 0.1
 	 */
-	public static function login(username:String, password:Bytes):Int {
+	public static function login(username:String, password:Bytes, url:String):Int {
 		var code = FAILURE;
+
+		var req = new Http(url);
+		var c:Request = {
+			status: NULL,
+			actions: [
+				{
+					instruction: LOGIN,
+					params: {
+						username: username,
+						password: password.toHex()
+					}
+				}
+			]
+		};
+		req.setPostData(Encryption.encrypt(Bytes.ofString(Json.stringify(c)), null).toHex());
+		req.onData = function(raw:String) {
+			var data:Request = Json.parse(Encryption.decrypt(Bytes.ofHex(raw), null).toString());
+			code = data.status;
+		}
 
 		return code;
 	}
