9cef5813c6e446b698f2aead0dc788b9c9ea4caf
Sid <saytleader@outlook.com>
2023-08-08 19:14:06 +0530

    Create Sender.hx

---
+++ src/prism/chat/Sender.hx
@@ -0,0 +1,28 @@
+package prism.chat;
+
+import prism.crypto.Encryption;
+import haxe.io.Bytes;
+import haxe.Json;
+import prism.util.Instructions.*;
+import prism.util.structs.Request;
+import haxe.Http;
+import prism.util.structs.Message;
+
+class Sender {
+	public static function send(message:Message, url:String) {
+		var req = new Http(url);
+		var c:Request = {
+			status: NULL,
+			actions: [
+				{
+					instruction: MESSAGE,
+					params: {
+						data: message
+					}
+				}
+			]
+		};
+		req.setPostData(Json.stringify(c));
+		req.request(true);
+	}
+}
