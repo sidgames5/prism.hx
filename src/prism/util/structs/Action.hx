a029199f56602c760b806ce2182c331e45f2f0a4
Sid <saytleader@outlook.com>
2023-08-13 13:46:51 +0530

    Update Action.hx

--- src/prism/util/structs/Action.hx
+++ src/prism/util/structs/Action.hx
@@ -1,15 +1,14 @@
 package prism.util.structs;
 
-import prism.util.structs.Message;
 import haxe.io.Bytes;
-import prism.util.structs.Message
+import prism.util.structs.Message;
 
 typedef Action = {
 	instruction:Int,
 	?params:{
 		?username:String,
 		?password:String,
-		?data:Any,
-	  ?messages:Array<Message>
-	}
+		?data:Any
+	},
+	?messages:Array<Message>
 }
